fc15602a1a5f5c90e92f290b297f0f015db6a347
Matt Dowdney and Pablo Barreyro <pablo@>
2018-03-22 14:37:02 -0700

    add html content, jquery logic and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
+  $("#check").submit(function(event){
+    event.preventDefault();
+    var sideA = parseInt($("#sideA").val());
+    var sideB = parseInt($("#sideB").val());
+    var sideC = parseInt($("#sideC").val());
+    var test1 = (sideA + sideB) - sideC;
+    var test2 = (sideB + sideC) - sideA;
+    var test3 = (sideA + sideC) - sideB;
 
+
+    if (test1 > 0 && test2 > 0 && test3 > 0){
+      if (sideA === sideB && sideB === sideC){
+        $("#triangletype").text("Equilateral");
+      } else if (sideA === sideB || sideB === sideC || sideA === sideC) {
+        $("#triangletype").text("Isosceles");
+      } else {
+        $("#triangletype").text("Scalene");
+      }
+    } else {
+      $("#triangletype").text("NOT A TRIANGLE");
+    }
+  });
 });
